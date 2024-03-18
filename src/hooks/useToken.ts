58f0e7c9d66bd237f361bd7131c440ce9eb457a0
Sparsh Verma <sparshv70@gmail.com>
2024-03-18 21:57:52 +0530

    Check:3

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -18,12 +18,12 @@ export const useToken = () => {
     const doesNotRequireAuthentication = unauthenticatedRoutes.includes(
       location.pathname
     );
-
+    console.log("Token: ", token);
     if (isSignedIn && doesNotRequireAuthentication) {
-      console.log("Going to dashboard now");
+      console.log("Going to dashboard now: ");
       navigate("/dashboard");
     } else if (!isSignedIn && requiresAuthentication) {
-      console.log("Going to login now");
+      console.log("Going to login now: ");
       navigate("/login");
     }
   }, [navigate, location.pathname]);
